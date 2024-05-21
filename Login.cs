44abac119f150d9cb4213c35fd588541aef2b29b
João Varela <joaovarela@ua.pt>
2024-05-21 22:10:16 +0100

    login form

---
+++ Login.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_BD_Tattoos
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Rececionista rececionista = new Rececionista();
+            rececionista.Show();
+            this.Hide();
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Artista artista = new Artista();
+            artista.Show();
+            this.Hide();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Cliente cliente = new Cliente();
+            cliente.Show();
+            this.Hide();
+        }
+    }
+}
--- Rececionista.cs
+++ Rececionista.cs
@@ -10,21 +10,11 @@ using System.Windows.Forms;
 
 namespace Project_BD_Tattoos
 {
-    public partial class Form1 : Form
+    public partial class Rececionista : Form
     {
-        public Form1()
+        public Rececionista()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            this.Hide();
         }
     }
 }
